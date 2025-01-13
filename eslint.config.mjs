7c526daca78bbd4331f0b7c70644d2b7e598b5d6
kybaq <flfsk12@gmail.com>
2025-01-13 20:43:33 +0900

    chore: eslint.config.js 설정 변경

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,60 +1,19 @@
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import globals from "globals";
-import tsParser from "@typescript-eslint/parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+// @ts-check
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
 
-export default [...compat.extends(
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
-), {
-    plugins: {
-        "@typescript-eslint": typescriptEslint,
-    },
-
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.recommendedTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
+  {
     languageOptions: {
-        globals: {
-            ...globals.browser,
-            ...globals.node,
-        },
-
-        parser: tsParser,
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
-
-    settings: {
-        "import/resolver": {
-            typescript: {},
-        },
-    },
-
-    rules: {
-        "@typescript-eslint/explicit-module-boundary-types": "warn",
-        "@typescript-eslint/no-explicit-any": "warn",
-
-        "@typescript-eslint/no-unused-vars": ["error", {
-            argsIgnorePattern: "^_",
-        }],
-
-        "@typescript-eslint/no-non-null-assertion": "warn",
-        "no-console": "warn",
-        "no-debugger": "warn",
-        "no-duplicate-imports": "error",
-        "no-unused-expressions": "error",
-        "no-var": "error",
-        "prefer-const": "error",
-        "prefer-arrow-callback": "error",
-    },
-}, {
-    files: ["**/*.ts", "**/*.tsx"],
-}];
+    ignores: ['dist/**/*.ts', 'dist/**', '**/*.mjs', 'eslint.config.mjs', '**/*.js'],
+  },
+);
