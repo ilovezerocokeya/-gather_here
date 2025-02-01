7489fbf1ec50b5e26279a5e843f73e43a424defb
kybaq <flfsk12@gmail.com>
2025-02-01 14:56:30 +0900

    feat: 서버 측에서도 create client 호출 시 타입 추론 자동적으로 가능하도록 추가

--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -1,10 +1,11 @@
+import { Database } from "@/types/supabase";
 import { createServerClient, type CookieOptions } from "@supabase/ssr";
 import { cookies } from "next/headers";
 
 export function createClient() {
   const cookieStore = cookies();
 
-  return createServerClient(
+  return createServerClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
@@ -14,13 +15,10 @@ export function createClient() {
         },
         setAll(cookiesToSet) {
           try {
-            cookiesToSet.forEach(({ name, value, options }) =>
-              cookieStore.set(name, value, options)
-            );
-          } catch {
-          }
+            cookiesToSet.forEach(({ name, value, options }) => cookieStore.set(name, value, options));
+          } catch {}
         },
       },
-    }
+    },
   );
 }
