479b4e948ac81161d9bd3117f73f29d4fd141cfe
kybaq <flfsk12@gmail.com>
2025-02-04 17:13:33 +0900

    feat: 검색 결과 제공 페이지 추가

---
+++ src/app/search/[keyword]/page.tsx
@@ -0,0 +1,28 @@
+import { createClient } from "@/utils/supabase/server";
+import SearchResultCard from "../components/SearchResultCard";
+
+const page = async ({ params }: { params: { keyword: string } }) => {
+  const supabase = createClient();
+  const keyword = decodeURIComponent(params.keyword);
+
+  let { data, error } = await supabase
+    .from("Posts")
+    .select("*")
+    .or(`title.ilike.${keyword}, content.ilike.%${keyword}%`);
+
+  if (error) return <div>데이터를 불러오는 데 오류가 발생했습니다.</div>;
+
+  return (
+    <>
+      <div> 검색어:{keyword} </div> <div> Figma 참고해 검색 창 보여주기 </div>
+      <div>스터디 / 프로젝트 IT 행사 허브 탭 만들기</div>
+      <div>
+        {data?.map((post) => (
+          <SearchResultCard post={post} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default page;
