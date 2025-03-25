7bfc965e9fc0d60a8118799d999416f1d0863acb
kybaq <flfsk12@gmail.com>
2025-03-25 11:49:51 +0900

    feat: 검색 결과 제공 페이지 서버 컴포넌트로 구현

--- src/app/search/[keyword]/page.tsx
+++ src/app/search/[keyword]/page.tsx
@@ -1,5 +1,6 @@
 import { createClient } from "@/utils/supabase/server";
 import SearchResultCard from "../components/SearchResultCard";
+import SearchBar from "@/components/Search/SearchBar";
 
 const page = async ({ params }: { params: { keyword: string } }) => {
   const supabase = createClient();
@@ -10,20 +11,24 @@ const page = async ({ params }: { params: { keyword: string } }) => {
     .select("*")
     .or(`title.ilike.${keyword}, content.ilike.%${keyword}%`);
 
-  if (error) return <div>데이터를 불러오는 데 오류가 발생했습니다.</div>;
+  // NOTE: Next.js 의 error.js 사용해봐도 좋을듯
+  if (error) return <div>데이터를 불러오는 데 오류가 발생했습니다. 다시 시도해 보세요.</div>;
 
   return (
-    <>
-      <div> 검색어:{keyword} </div> <div> Figma 참고해 검색 창 보여주기 </div>
+    <div className="flex flex-col justify-between items-center">
       <div>스터디 / 프로젝트 IT 행사 허브 탭 만들기</div>
       <div>
-        {data?.map((post) => (
-          <li key={post.post_id}>
-            <SearchResultCard post={post} />
-          </li>
-        ))}
+        {data?.length ? (
+          data?.map((post) => (
+            <li key={post.post_id}>
+              <SearchResultCard post={post} />
+            </li>
+          ))
+        ) : (
+          <div>검색 결과가 없습니다.</div>
+        )}
       </div>
-    </>
+    </div>
   );
 };
 
