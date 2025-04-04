0eecbeea8c75627f3991b5f522713723e93d5dd9
kybaq <flfsk12@gmail.com>
2025-04-05 01:10:15 +0900

    fix: 콜백 함수의 파라미터 type 추론하도록 변경

--- src/hooks/useChat.ts
+++ src/hooks/useChat.ts
@@ -1,31 +1,30 @@
-import { useState, useEffect, useRef, FormEvent, KeyboardEvent } from "react";
-import { useAuth } from "@/provider/user/UserAuthProvider";
-import { supabase } from "@/utils/supabase/client";
-import { MessageRow } from "@/types/chats/Chats.type";
+import { useState, useEffect, useRef, FormEvent, KeyboardEvent } from 'react';
+import { useAuth } from '@/provider/user/UserAuthProvider';
+import { supabase } from '@/utils/supabase/client';
+import { MessageRow } from '@/types/chats/Chats.type';
 
-const debounce = (func: Function, delay: number) => {
-  let timeoutId: NodeJS.Timeout;
-  return (...args: any[]) => {
+function debounce<T extends (...args: never[]) => unknown>(func: T, delay: number) {
+  let timeoutId: ReturnType<typeof setTimeout>;
+  return (...args: Parameters<T>): void => {
     clearTimeout(timeoutId);
     timeoutId = setTimeout(() => func(...args), delay);
   };
-};
+}
 
 const useChat = () => {
   const { user } = useAuth();
   const [messages, setMessages] = useState<MessageRow[]>([]);
-  const [inputValue, setInputValue] = useState<string>("");
+  const [inputValue, setInputValue] = useState<string>('');
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
   const chatContentDivRef = useRef<HTMLDivElement>(null);
   const formRef = useRef<HTMLFormElement>(null);
 
   useEffect(() => {
-
     const getAllMessages = async () => {
       const { data: messages, error } = await supabase
-        .from("Messages")
+        .from('Messages')
         .select(`*, Users (nickname, profile_image_url)`)
-        .order("sent_at", { ascending: true });
+        .order('sent_at', { ascending: true });
 
       if (error) {
         console.error(error);
@@ -41,36 +40,36 @@ const useChat = () => {
       }
     };
 
-    getAllMessages();
+    void getAllMessages();
 
     const openTalkSubscription = supabase
-      .channel("openTalk")
+      .channel('openTalk')
       .on(
-        "postgres_changes",
+        'postgres_changes',
         {
-          event: "INSERT",
-          schema: "public",
-          table: "Messages",
+          event: 'INSERT',
+          schema: 'public',
+          table: 'Messages',
         },
         () => {
-          getAllMessages();
+          void getAllMessages();
         },
       )
       .on(
-        "postgres_changes",
+        'postgres_changes',
         {
-          event: "DELETE",
-          schema: "public",
-          table: "Messages",
+          event: 'DELETE',
+          schema: 'public',
+          table: 'Messages',
         },
         () => {
-          getAllMessages();
+          void getAllMessages();
         },
       )
       .subscribe();
 
     return () => {
-      openTalkSubscription.unsubscribe();
+      void openTalkSubscription.unsubscribe();
     };
   }, []);
 
@@ -80,29 +79,28 @@ const useChat = () => {
     if (!user) {
       return;
     } else if (formRef.current && inputValue.trim()) {
-
       const { error } = await supabase
-        .from("Messages")
+        .from('Messages')
         .insert({
-          channel_id: "214322ba-1cbd-424c-9ef1-e4b281f71675",
+          channel_id: '214322ba-1cbd-424c-9ef1-e4b281f71675',
           user_id: `${user.id}`,
           content: inputValue,
         })
-        .select("*");
+        .select('*');
 
       if (error) {
-        console.error("에러: ", error);
+        console.error('에러: ', error);
         return;
       }
 
-      setInputValue("");
+      setInputValue('');
     }
   };
 
   const debouncedSubmit = debounce(handleSubmit, 300);
 
   const handleEnterKeyDown = (evt: KeyboardEvent<HTMLTextAreaElement>) => {
-    if (!evt.shiftKey && evt.key === "Enter") {
+    if (!evt.shiftKey && evt.key === 'Enter') {
       evt.preventDefault();
       if (formRef.current && inputValue.trim()) {
         debouncedSubmit();
@@ -113,11 +111,10 @@ const useChat = () => {
   };
 
   const handleDelete = async (message_id: string) => {
-
-    const { error } = await supabase.from("Messages").delete().eq("message_id", message_id);
+    const { error } = await supabase.from('Messages').delete().eq('message_id', message_id);
 
     if (error) {
-      console.error("에러: ", error);
+      console.error('에러: ', error);
       return;
     }
   };
