8df814aa423d83e9b28fe2b9945e8a2a93e08dd7
kybaq <flfsk12@gmail.com>
2025-04-05 00:56:01 +0900

    fix: 전달받은 props 의 올바른 속성을 사용하도록 수정

--- src/components/MainDetail/LikeButton.tsx
+++ src/components/MainDetail/LikeButton.tsx
@@ -1,20 +1,20 @@
-"use client";
+'use client';
 
-import React, { useState, useEffect } from "react";
-import { supabase } from "@/utils/supabase/client";
-import Toast from "@/components/Common/Toast/Toast";
-
+import React, { useState, useEffect } from 'react';
+import { supabase } from '@/utils/supabase/client';
+import Toast from '@/components/Common/Toast/Toast';
+import { UserData } from '@/types/userData';
 
 interface LikeButtonProps {
   postId: string;
-  currentUser: any;
+  currentUser: UserData | null;
   category: string;
   onRemoveBookmark?: (postId: string) => void;
 }
 
 const LikeButton: React.FC<LikeButtonProps> = ({ postId, currentUser, category, onRemoveBookmark }) => {
   const [liked, setLiked] = useState(false);
-  const [toastState, setToastState] = useState({ state: "", message: "" });
+  const [toastState, setToastState] = useState({ state: '', message: '' });
 
   useEffect(() => {
     const checkLikeStatus = async () => {
@@ -23,12 +23,12 @@ const LikeButton: React.FC<LikeButtonProps> = ({ postId, currentUser, category, 
           data: likeData,
           error: likeError,
           status,
-        } = await supabase.from("Interests").select("*").eq("user_id", currentUser.id).eq("post_id", postId);
+        } = await supabase.from('Interests').select('*').eq('user_id', currentUser.user_id).eq('post_id', postId);
 
         if (likeError) {
           if (status !== 406) {
-            console.error("Error checking like status:", likeError.message);
-            setToastState({ state: "error", message: "좋아요 상태를 확인하는 중 오류가 발생했습니다." });
+            console.error('Error checking like status:', likeError.message);
+            setToastState({ state: 'error', message: '좋아요 상태를 확인하는 중 오류가 발생했습니다.' });
           }
         } else if (likeData && likeData.length > 0) {
           setLiked(true);
@@ -36,36 +36,40 @@ const LikeButton: React.FC<LikeButtonProps> = ({ postId, currentUser, category, 
       }
     };
 
-    checkLikeStatus();
+    void checkLikeStatus();
   }, [currentUser, postId]);
 
   const handleLike = async () => {
     if (!currentUser) {
-      setToastState({ state: "error", message: "로그인이 필요합니다!" });
+      setToastState({ state: 'error', message: '로그인이 필요합니다!' });
       return;
     }
 
     if (!liked) {
-      const { error } = await supabase.from("Interests").insert({
-        user_id: currentUser.id,
+      const { error } = await supabase.from('Interests').insert({
+        user_id: currentUser.user_id,
         post_id: postId,
         category,
       });
       if (error) {
-        setToastState({ state: "error", message: "좋아요 등록 중 오류가 발생했습니다." });
-        console.error("Error liking post:", error.message);
+        setToastState({ state: 'error', message: '좋아요 등록 중 오류가 발생했습니다.' });
+        console.error('Error liking post:', error.message);
       } else {
         setLiked(true);
-        setToastState({ state: "success", message: "게시글을 좋아요 했습니다!" });
+        setToastState({ state: 'success', message: '게시글을 좋아요 했습니다!' });
       }
     } else {
-      const { error } = await supabase.from("Interests").delete().eq("user_id", currentUser.id).eq("post_id", postId);
+      const { error } = await supabase
+        .from('Interests')
+        .delete()
+        .eq('user_id', currentUser.user_id)
+        .eq('post_id', postId);
       if (error) {
-        setToastState({ state: "error", message: "좋아요 취소 중 오류가 발생했습니다." });
-        console.error("Error unliking post:", error.message);
+        setToastState({ state: 'error', message: '좋아요 취소 중 오류가 발생했습니다.' });
+        console.error('Error unliking post:', error.message);
       } else {
         setLiked(false);
-        setToastState({ state: "success", message: "게시글 좋아요를 취소했습니다." });
+        setToastState({ state: 'success', message: '게시글 좋아요를 취소했습니다.' });
 
         if (onRemoveBookmark) {
           onRemoveBookmark(postId);
@@ -76,11 +80,11 @@ const LikeButton: React.FC<LikeButtonProps> = ({ postId, currentUser, category, 
 
   return (
     <>
-      <button type="button" onClick={handleLike} className="flex items-center justify-end w-[30px]">
+      <button type="button" onClick={() => void handleLike()} className="flex items-center justify-end w-[30px]">
         <svg width="20" height="20" viewBox="0 0 22 32" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path
             d="M17.2867 0H4.71526C2.04384 0 0 2.08 0 4.8V30.4C0 30.72 0 30.88 0.15812 31.2C0.629548 32 1.57241 32.16 2.35812 31.84L11.0005 26.72L19.6433 31.84C19.9581 32 20.1153 32 20.4291 32C21.372 32 22.0005 31.36 22.0005 30.4V4.8C22.0005 2.08 19.9581 0 17.2867 0Z"
-            fill={liked ? "#C3E88D" : "#5E5E5E"}
+            fill={liked ? '#C3E88D' : '#5E5E5E'}
           />
         </svg>
       </button>
@@ -88,7 +92,7 @@ const LikeButton: React.FC<LikeButtonProps> = ({ postId, currentUser, category, 
         <Toast
           state={toastState.state}
           message={toastState.message}
-          onClear={() => setToastState({ state: "", message: "" })}
+          onClear={() => setToastState({ state: '', message: '' })}
         />
       )}
     </>
