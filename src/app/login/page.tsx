6e291d49f18ded84c6efcacd8cd106c4bf6c4b69
kybaq <flfsk12@gmail.com>
2025-04-11 01:17:31 +0900

    chore: 경로 이동, intercepting / parellel routes 적용을 위해 route group 에서 분리

---
+++ src/app/login/page.tsx
@@ -0,0 +1,12 @@
+import LoginForm from '@/components/Login/LoginForm';
+
+const LoginPage: React.FC = () => {
+  return (
+    <>
+      <p>page</p>
+      <LoginForm />
+    </>
+  );
+};
+
+export default LoginPage;
