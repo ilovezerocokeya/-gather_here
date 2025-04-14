d1a74912edd81ca1d8fc91936119399588765995
ilovezerocokeya <98097563+ilovezerocokeya@users.noreply.github.com>
2025-04-14 14:40:04 +0900

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,8 @@
 import { type NextRequest } from "next/server";
 import { updateSession } from "@/utils/supabase/middleware";
 
-export function middleware(request: NextRequest) {
-  return  updateSession(request);
+export async function middleware(request: NextRequest) {
+  return await updateSession(request);
 }
 
 export const config = {
