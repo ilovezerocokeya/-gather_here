462bc05273a0b8d7409b501f11bf20a2a38f4688
ilovezerocokeya <147008077a@gmail.com>
2025-05-29 23:43:43 +0900

    fix: LoginPage수정

--- src/app/@login/(.)login/page.tsx
+++ src/app/@login/(.)login/page.tsx
@@ -1,11 +1,8 @@
-import LoginForm from '@/components/Login/LoginForm';
-import LoginModals from '../../../components/Common/Modal/modal';
+import LoginModal from '../../../components/Common/Modal/modal';
 
 const LoginPage: React.FC = () => {
   return (
-    <LoginModals>
-      <LoginForm />
-    </LoginModals>
+    <LoginModal />
   );
 };
 
